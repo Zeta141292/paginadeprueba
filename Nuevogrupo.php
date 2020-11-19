75ba6488b2f2fcb4e48e6c3d26a1704b2306d157
Zeta141292 <74737961+Zeta141292@users.noreply.github.com>
2020-11-19 15:05:37 -0500

    Add files via upload

---
+++ Nuevogrupo.php
@@ -0,0 +1,19 @@
+<HTML>
+<HEAD>
+
+<link rel="stylesheet" type="text/css" href="css/style.css"
+</HEAD>
+<BODY>
+		
+
+		
+<form action="comprueba_login.php"method="post">
+	
+
+
+<div id="contenido40">
+<img src="imagenes/captura1.png" width="29" height=29/>  
+</div><br>
+
+</body>
+</html>
